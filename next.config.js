27c76ae68494fc948938b7c1e94e58b45a437b56
Prachi Vishnoi <prachi27vishnoi@gmail.com>
2021-05-07 12:23:47 +0530

    added middleware & token authentication

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   env: {
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/prachicloud/image/upload"
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
   }
 };
